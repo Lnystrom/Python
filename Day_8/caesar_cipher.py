53e21d445b42f1630ac8a8a6041b50767cbf253d
Nystrom <apleuryssomrs@gmail.com>
2023-07-07 11:30:49 -0300

    Caesar Cipher finished

--- Day_8/caesar_cipher.py
+++ Day_8/caesar_cipher.py
@@ -1,23 +1,31 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(art.logo)
 def caeser(text, shift, direction):
     if direction == "encode":
         cipher_text = ""
-        for letter in text:
-            index = alphabet.index(letter)
-            new_index = index + shift
-            if new_index >len(alphabet):
-                new_index = new_index%len(alphabet)
-            cipher_text += alphabet[new_index]
+        for char in text:
+            if char in alphabet:
+                index = alphabet.index(char)
+                new_index = index + shift
+                if new_index >len(alphabet):
+                    new_index = new_index%len(alphabet)
+                cipher_text += alphabet[new_index]
+            else:
+                cipher_text+=char    
         print(f"The encoded text is {cipher_text}")
     
     elif direction == "decode":
         cipher_text = ""
-        for letter in text:
-            index = alphabet.index(letter)
-            new_index = index - shift
-            cipher_text += alphabet[new_index]
+        for char in text:
+            if char in alphabet:
+                index = alphabet.index(char)
+                new_index = index - shift%len(alphabet)
+                cipher_text += alphabet[new_index]
+            else:
+                cipher_text+=char 
         print(f"The decoded text is {cipher_text}")
 
 finished = False
@@ -28,6 +36,7 @@ while not finished:
     caeser(text, shift, direction)
     question = input("Do you want to do it again? 'Yes' or 'No'")
     if question.lower() == "no":
+        print("Goodbye!")
         finished = True
     else:
         caeser(text, shift, direction)   
