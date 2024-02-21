c4057a3fad0c980da198d52ab286e75e8d851f5f
Nystrom <apleuryssomrs@gmail.com>
2024-02-21 01:40:05 -0300

    Now it shows the questions

--- Day_17/quiz-game-start/main.py
+++ Day_17/quiz-game-start/main.py
@@ -10,3 +10,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- Day_17/quiz-game-start/quiz_brain.py
+++ Day_17/quiz-game-start/quiz_brain.py
@@ -1,5 +1,3 @@
-from main import question_bank
-
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -7,4 +5,5 @@ class QuizBrain:
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        input(f"Q. {self.question_number}: {current_question.text}")
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
