38998916cadb2931a87e288e500b9cac7ff456b9
Nystrom <apleuryssomrs@gmail.com>
2024-02-22 19:31:03 -0300

    Feito um quadrado utilizando turtle

--- Day_18/main.py
+++ Day_18/main.py
@@ -2,14 +2,18 @@ from turtle import Turtle, Screen
 
 #para saber como usa deve-se pesquisar a documentação da API
 #é uma boa ideia utilizar StackOverflow
+
+
+
 #aparência
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("arrow")
 timmy_the_turtle.color("honeydew2")
 
 #movimentação
-timmy_the_turtle.forward(300)
-timmy_the_turtle.right()
+for _ in range(4):
+    timmy_the_turtle.forward(50)
+    timmy_the_turtle.right(90)
 
 #isso deve ocorrer depois de criar a tartaruga
 screen = Screen()
