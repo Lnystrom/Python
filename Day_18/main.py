ae30900d32096917ef3d23d3fbddafa29edcdcab
Nystrom <apleuryssomrs@gmail.com>
2024-02-22 20:39:58 -0300

    Desafio 4: Fazer vários polígonos utilizando o ângulo externo como referência

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,24 +1,26 @@
 from turtle import Turtle, Screen
+from random import choice
 #from turtle import * // Importa todas as classes dentro do módulo
 
 #para saber como usa deve-se pesquisar a documentação da API
 #é uma boa ideia utilizar StackOverflow
 
+cores = ["purple", "black", "blue", "red", "green", "orange", "pink", "yellow"]
 
 
 #aparência
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("arrow")
-timmy_the_turtle.color("honeydew2")
+
 
 #movimentação
-for _ in range(30):
-    timmy_the_turtle.forward(30)
-    if _%2 == 0:
-        timmy_the_turtle.pendown()
-    else:
-        timmy_the_turtle.penup()
-   
+for side in range(3, 50):
+    timmy_the_turtle.color(cores[(side - 3)%len(cores)])
+    for walks in range(side):
+        timmy_the_turtle.forward(25)
+        timmy_the_turtle.right(360/side)
+        
+
 
 #isso deve ocorrer depois de criar a tartaruga
 screen = Screen()
