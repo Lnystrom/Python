55a39aafac6035a6416b9d6682133426e726f48a
Nystrom <apleuryssomrs@gmail.com>
2024-02-22 21:21:35 -0300

    Desafio 4: Gerar uma trajetória aleatória

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,26 +1,24 @@
 from turtle import Turtle, Screen
-from random import choice
+from random import choice, randrange
 #from turtle import * // Importa todas as classes dentro do módulo
 
 #para saber como usa deve-se pesquisar a documentação da API
 #é uma boa ideia utilizar StackOverflow
 
 cores = ["purple", "black", "blue", "red", "green", "orange", "pink", "yellow"]
+directions = [0, 90, 180, 270]
 
 
 #aparência
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("arrow")
+timmy_the_turtle.shape("square")
+timmy_the_turtle.speed(6)
 
-
-#movimentação
-for side in range(3, 50):
-    timmy_the_turtle.color(cores[(side - 3)%len(cores)])
-    for walks in range(side):
-        timmy_the_turtle.forward(25)
-        timmy_the_turtle.right(360/side)
-        
-
+for walk in range (0,100):
+    timmy_the_turtle.pensize(randrange(10))
+    timmy_the_turtle.color(cores[(walk)%len(cores)])
+    timmy_the_turtle.right(choice(directions))
+    timmy_the_turtle.forward(40)
 
 #isso deve ocorrer depois de criar a tartaruga
 screen = Screen()
