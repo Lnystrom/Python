a70c3085723e79e49af91aab58168c550c14b140
Nystrom <apleuryssomrs@gmail.com>
2024-02-22 21:50:14 -0300

    Usando tuples e criando um gerador aleatório de cores

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,13 +1,19 @@
-from turtle import Turtle, Screen
-from random import choice, randrange
+from turtle import Turtle, Screen, colormode
+import random
 #from turtle import * // Importa todas as classes dentro do módulo
 
 #para saber como usa deve-se pesquisar a documentação da API
 #é uma boa ideia utilizar StackOverflow
 
-cores = ["purple", "black", "blue", "red", "green", "orange", "pink", "yellow"]
 directions = [0, 90, 180, 270]
+colormode(255)
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    arco_iris = (r, g, b)
+    return arco_iris
 
 #aparência
 timmy_the_turtle = Turtle()
@@ -15,9 +21,9 @@ timmy_the_turtle.shape("square")
 timmy_the_turtle.speed(6)
 
 for walk in range (0,100):
-    timmy_the_turtle.pensize(randrange(10))
-    timmy_the_turtle.color(cores[(walk)%len(cores)])
-    timmy_the_turtle.right(choice(directions))
+    timmy_the_turtle.pensize(10)
+    timmy_the_turtle.color(random_color())
+    timmy_the_turtle.right(random.choice(directions))
     timmy_the_turtle.forward(40)
 
 #isso deve ocorrer depois de criar a tartaruga
